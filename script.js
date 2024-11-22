0e4a445c2934bcd1cc69cab16fabee023640e1c7
Valyhdh <val.ronik1@gmail.com>
2024-11-23 02:46:33 +0500

    3.2
    Add swiper and add button (show/hidden)

--- script.js
+++ script.js
@@ -83,63 +83,142 @@ feedbackDisn.addEventListener('click', function () {
     feedback.classList.remove('modal--show');
 });
 
-new Swiper('.swiper', {
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1.2,
-        },
-        350: {
-            slidesPerView: 1.3,
-        },
-        380: {
-            slidesPerView: 1.4,
-        },
-        410: {
-            slidesPerView: 1.5,
-        },
-        440: {
-            slidesPerView: 1.6,
-        },
-        470: {
-            slidesPerView: 1.7,
-        },
-        500: {
-            slidesPerView: 1.9,
-        },
-        530: {
-            slidesPerView: 2.0,
-        },
-        560: {
-            slidesPerView: 2.1,
-        },
-        590: {
-            slidesPerView: 2.2,
-        },
-        610: {
-            slidesPerView: 2.3,
-        },
-        640: {
-            slidesPerView: 2.4,
-        },
-        670: {
-            slidesPerView: 2.5,
-        },
-        700: {
-            slidesPerView: 2.6,
-        },
-        730: {
-            slidesPerView: 2.7,
-        },
-        760: {
-            slidesPerView: 2.8,
-        },
 
-    },
-    slidesOffsetAfter: 75,
-    spaceBerween: 20,
+window.addEventListener('DOMContentLoaded', () => {
+
+    const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+        let swiper;
+
+        breakpoint = window.matchMedia(breakpoint);
+
+        const enableSwiper = function (className, settings) {
+            swiper = new Swiper(className, settings);
+
+            if (callback) {
+                callback(swiper);
+            }
+        }
+
+        const checker = function () {
+            if (breakpoint.matches) {
+                return enableSwiper(swiperClass, swiperSettings);
+            } else {
+                if (swiper !== undefined) swiper.destroy(true, true);
+                return;
+            }
+        };
+
+        breakpoint.addEventListener('change', checker);
+        checker();
+    }
+
+    const someFunc = (instance) => {
+        if (instance) {
+            instance.on('slideChange', function (e) {
+                console.log('*** mySwiper.activeIndex', instance.activeIndex);
+            });
+        }
+    };
+
+    resizableSwiper(
+        '(max-width: 767px)',
+        '.swiper__brend',
+        {
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1.2,
+                },
+                350: {
+                    slidesPerView: 1.3,
+                },
+                380: {
+                    slidesPerView: 1.4,
+                },
+                410: {
+                    slidesPerView: 1.5,
+                },
+                440: {
+                    slidesPerView: 1.6,
+                },
+                470: {
+                    slidesPerView: 1.7,
+                },
+                500: {
+                    slidesPerView: 1.9,
+                },
+                530: {
+                    slidesPerView: 2.0,
+                },
+                560: {
+                    slidesPerView: 2.1,
+                },
+                590: {
+                    slidesPerView: 2.2,
+                },
+                610: {
+                    slidesPerView: 2.3,
+                },
+                640: {
+                    slidesPerView: 2.4,
+                },
+                670: {
+                    slidesPerView: 2.5,
+                },
+                700: {
+                    slidesPerView: 2.6,
+                },
+                730: {
+                    slidesPerView: 2.7,
+                },
+                760: {
+                    slidesPerView: 2.8,
+                },
+
+            },
+            slidesOffsetAfter: 75,
+            spaceBerween: 20,
+        },
+        someFunc
+    );
 });
+
+const mainText = document.querySelector('.content__main-text');
+const mainTextDbn = document.querySelector('.content__more_text');
+const brendsCard = document.querySelector('.swiper__brend');
+const brendsCardDbn = document.querySelector('.content__more_brends');
+
+mainTextDbn.addEventListener('click', function mainTextShow() {
+    
+    if (mainTextDbn.innerHTML == "Читать далее")
+    {
+        mainTextDbn.innerHTML = "Скрыть";
+        mainText.style.height = "auto";
+        mainTextDbn.classList.add("content__more_text--show")
+    }
+    else {
+        mainTextDbn.innerHTML = "Читать далее";
+        mainText.style.height = "80px";
+        mainTextDbn.classList.remove("content__more_text--show")
+    }
+});
+
+brendsCardDbn.addEventListener('click', function brendsShow() {
+    
+    if (brendsCardDbn.innerHTML == "Показать все")
+    {
+        brendsCardDbn.innerHTML = "Скрыть";
+        brendsCard.style.height = "auto";
+        brendsCardDbn.classList.add("content__more_brends--show")
+    }
+    else {
+        brendsCardDbn.innerHTML = "Показать все";
+        brendsCard.style.height = "80px";
+        brendsCardDbn.classList.remove("content__more_brends--show")
+    }
+});
+
 
